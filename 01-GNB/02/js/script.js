8ef50fab89d931b7910f69a6aa6ca7933b9cf8d3
haribo28 <soyoonk0625@naver.com>
2024-10-04 10:54:02 +0900

    찐막

--- 01-GNB/02/js/script.js
+++ 01-GNB/02/js/script.js
@@ -21,13 +21,16 @@ $(function () {
     // 마우스 휠을 조작했을 때
     $window.on("wheel", function (e) {
         // console.log(e);
+        // if (e.originalEvent.wheelDelta > 0) {
+        //     // 휠을 올렸을 때
+        //     $header.removeClass("hide");
+        // } else {
+        //     // 휠을 내렸을 때
+        //     $header.addClass("hide");
+        // }
 
-        if (e.originalEvent.wheelDelta > 0) {
-            // 휠을 올렸을 때
-            $header.removeClass("hide");
-        } else {
-            // 휠을 내렸을 때
-            $header.addClass("hide");
-        }
+        // 삼항조건연산자
+        // 조건 ? 참일 때 동작 : 거짓일 때 동작
+        e.originalEvent.wheelDelta > 0 ? $header.removeClass("hide") : $header.addClass("hide");
     });
 });
